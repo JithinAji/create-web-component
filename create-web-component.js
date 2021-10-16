30dc4fa9a0e4d0a9ae525911395c6a94f9152719
JithinAji <jithin_aji@outlook.com>
2021-10-16 23:40:43 +0530

    invalid format error message added

--- create-web-component.js
+++ create-web-component.js
@@ -9,6 +9,11 @@ let componentName = "";
 
 const args = process.argv.slice(2);
 
+if (args.length != 1) {
+  console.log("use format: node create-web-compoent component-name ");
+  process.exit(1);
+}
+
 args.forEach((arg) => {
   if (arg.indexOf("name=") == 0) {
     componentName = arg.replace("name=", "");
